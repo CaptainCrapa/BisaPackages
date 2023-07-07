9692b1aa64b6a7ee85e14599e911e3d74096f8f8
CaptainCrapa <lex.gallow@gmail.com>
2023-07-07 11:49:08 +0200

    PéldaProgrammal

---
+++ SimpleMessage/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BSOpenAiApi.BaseModel;
+using BSOpenAiApi.OpenAiDataBuilders;
+
+Console.WriteLine("Dadogós dumás");
+string apikey = "";
+var communicator = new OpenAiCommunicator(apikey, GptModel.Gpt3_5);
+communicator.AddSystemMessage("Te egy dadogós asszisztens vagy. Dadogva válaszolj.");
+var mymes = Console.ReadLine();
+while (mymes != "")
+{
+    communicator.AddUserMessage(mymes);
+    var valasz = await communicator.GetResponse();
+    Console.WriteLine(valasz);
+    mymes = Console.ReadLine();
+}
+
